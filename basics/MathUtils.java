c1830a4bdb5f73378e682d29a56d3f3a009d73ab
liunanqi <liunanqizero@outlook.com>
2019-03-20 14:49:40 +0800

    modified mathutils

--- basics/MathUtils.java
+++ basics/MathUtils.java
@@ -1,4 +1,12 @@
 public class MathUtils {
+    public static int[] generateIntArray(){
+        return generateIntArray(10);
+    }
+
+    public static int[] generateIntArray(int length){
+        return generateIntArray(length, 10);
+    }
+
     public static int[] generateIntArray(int length, int range){
         int[] result = new int[length];
         for(int i = 0; i < result.length; i++){
@@ -6,5 +14,4 @@ public class MathUtils {
         }
         return result;
     }
-
 }
