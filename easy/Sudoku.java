491b7585f1d6630033f0dd88ef435f2965334c64
liunanqi <liunanqizero@outlook.com>
2019-03-20 21:17:15 +0800

    BST problem and other two practices

---
+++ easy/Sudoku.java
@@ -0,0 +1,42 @@
+public class Sudoku {
+    public static void main(String[] args){
+        char[][] board = new char[][]{
+                {'5','3','.','.','7','.','.','.','.'},
+                {'6','.','.','1','9','5','.','.','.'},
+                {'.','9','8','.','.','.','.','6','.'},
+                {'8','.','.','.','6','.','.','.','3'},
+                {'4','.','.','8','.','3','.','.','1'},
+                {'7','.','.','.','2','.','.','.','6'},
+                {'.','6','.','.','.','.','2','8','.'},
+                {'.','.','.','4','1','9','.','.','5'},
+                {'.','.','.','.','8','.','.','7','9'}
+        };
+        boolean isValid = isValidSudoku(board);
+        System.out.println("Is this Sudoku valid?\n" + isValid);
+    }
+
+    public static boolean isValidSudoku(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            boolean[] row = new boolean[9];
+            boolean[] col = new boolean[9];
+            boolean[] box = new boolean[9];
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                } else if (row[board[i][j] - '1']) return false;
+                else row[board[i][j] - '1'] = true;
+
+                if (board[j][i] == '.') {
+                } else if (col[board[j][i] - '1']) return false;
+                else col[board[j][i] - '1'] = true;
+
+                int m = i / 3 * 3 + j / 3;
+                int n = i % 3 * 3 + j % 3;
+
+                if(board[m][n] == '.'){}
+                else if(box[board[m][n] - '1']) return false;
+                else box[board[m][n] - '1'] = true;
+            }
+        }
+        return true;
+    }
+}
---
+++ medium/MyAtoi.java
@@ -0,0 +1,32 @@
+public class MyAtoi {
+    public static void main(String[] args){
+        String str = "-91283472332";
+        System.out.println(String.valueOf(atoi(str)));
+    }
+    public static int atoi(String str){
+        str = str.trim();
+        if(str.length() == 0) return 0;
+        int sign = 1;
+        long sum = 0;
+        int i = 0;
+        char first = str.charAt(i);
+        if(first == '-'){
+            sign = -1;
+            i++;
+        }else if(first == '+'){
+            sign = 1;
+            i++;
+        }
+        for(; i < str.length(); i++){
+            if(!Character.isDigit(str.charAt(i))){
+                return (int)sum*sign;
+            }
+            sum = sum*10 + str.charAt(i) - '0';
+            if (sign == 1 && sum > Integer.MAX_VALUE)
+                return Integer.MAX_VALUE;
+            if (sign == -1 && (-1) * sum < Integer.MIN_VALUE)
+                return Integer.MIN_VALUE;
+        }
+        return (int)sum*sign;
+    }
+}
