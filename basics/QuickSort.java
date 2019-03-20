955cf5757813e5cbfba3194c649b7872d3c7be32
liunanqi <liunanqizero@outlook.com>
2019-03-20 10:52:37 +0800

    quick sort

--- basics/QuickSort.java
+++ basics/QuickSort.java
@@ -1,6 +1,6 @@
 public class QuickSort {
     public static void main(String[] args){
-        int[] eg = {3, 5, 1, 4, 6, 7, 2};
+        int[] eg = {3, 5, 1, 4, 6, 7, 2, 4, 3};
         quickSort(eg);
         for(int i = 0; i < eg.length; i++){
             System.out.println(eg[i]);
@@ -11,33 +11,42 @@ public class QuickSort {
         quickSort(list, 0, list.length - 1);
     }
 
-    public static void quickSort(int[] list, int low, int high) {
-        if(high <= low) return;
-        int pivotIndex = partition(list, low, high);
-        quickSort(list, low, pivotIndex - 1);
-        quickSort(list, pivotIndex + 1, high);
+    public static void quickSort(int[] list, int first, int last) {
+        if(last > first) {
+            int pivotIndex = partition(list, first, last);
+            quickSort(list, first, pivotIndex - 1);
+            quickSort(list, pivotIndex + 1, last);
+        }
     }
 
-    public static int partition(int[] list, int low, int high) {
-        int i = low, j = high + 1;
-        int pivot = list[low];
-        while(true){
-            while(list[++i] < pivot){
-                if(i == high)
-                    break;
+    public static int partition(int[] list, int first, int last) {
+        int pivot = list[first];
+        int low = first + 1;
+        int high = last;
+        while(high > low){
+            while(low <= high && list[low] < pivot){
+                low++;
+            }
+            while(low <= high && list[high] >= pivot){
+                high--;
             }
 
-            while(list[--j] > pivot){
-                if(j == low)
-                    break;
+            if(low < high){
+                exch(list, low, high);
             }
-            if(i >= j)
-                break;
-            exch(list, i, j);
         }
 
-        exch(list, low, j);
-        return j;
+//        while(high > first && list[high] >= pivot){
+//            high--;
+//        }
+
+        if(pivot > list[high]){
+            list[first] = list[high];
+            list[high] = pivot;
+            return high;
+        }else {
+            return first;
+        }
     }
 
     public static void exch(int[] list, int i, int j){
